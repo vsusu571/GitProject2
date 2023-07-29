f335b9633377cc7db9bc880d1a2ede8e1e1c3d51
zackw <zackw@AJ.lan>
2023-07-29 11:52:19 -0400

    Group Exce

---
+++ src/Group4/Testing.java
@@ -0,0 +1,16 @@
+package Group4;
+
+public class Testing {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Sue E");
+	}
+	
+public void add(int a , int b) {
+	
+	System.out.println(a+b);
+	
+}
+}
